eb04c34c129959c623098281896cf77b1277f6ee
celyes <celyes02@gmail.com>
2018-09-09 17:14:42 +0100

    Add columns to event_registers table migration

--- database/migrations/2018_09_09_160707_create_event_registers_table.php
+++ database/migrations/2018_09_09_160707_create_event_registers_table.php
@@ -15,6 +15,12 @@ class CreateEventRegistersTable extends Migration
     {
         Schema::create('event_registers', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('facebook');
+            $table->double('tel');
+            $table->string('email');
+            $table->string('message');
             $table->timestamps();
         });
     }
