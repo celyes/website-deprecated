096903f15adacdf42ac3d4f2949e4943b4fd4bc7
celyes <celyes02@gmail.com>
2018-09-09 13:59:13 +0100

    Add fields to the notifications table migration

--- database/migrations/2018_09_09_113818_create_notifications_table.php
+++ database/migrations/2018_09_09_113818_create_notifications_table.php
@@ -15,6 +15,8 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('notification');
+            $table->boolean('vue');
             $table->timestamps();
         });
     }
