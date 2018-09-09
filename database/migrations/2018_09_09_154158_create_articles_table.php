1673d572bd0a631118bbc1a70826b4b1bfd791a3
celyes <celyes02@gmail.com>
2018-09-09 16:45:49 +0100

    Add columns to article table migration

--- database/migrations/2018_09_09_154158_create_articles_table.php
+++ database/migrations/2018_09_09_154158_create_articles_table.php
@@ -15,6 +15,13 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('body');
+            $table->string('photo_principal');
+            $table->string('another_photo');
+            $table->int('view');
+            $table->int('vote');
+            $table->string('keyword');
             $table->timestamps();
         });
     }
