099965d54aa49722f94eae428a622fb1b890e93b
ihahachi <iha2007hachi@gmail.com>
2018-09-10 22:11:14 +0100

    Add columns Fk to articles migration

--- database/migrations/2018_09_10_205846_relation_articles_table.php
+++ database/migrations/2018_09_10_205846_relation_articles_table.php
@@ -13,7 +13,17 @@ class RelationArticlesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('articles', function (Blueprint $table) {
+            $table->integer('categorie_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            $table->foreign('categorie_id')
+            ->references('id')->on('categories')
+            ->onDelete('cascade');
+            $table->foreign('user_id')
+            ->references('id')->on('users')
+            ->onDelete('cascade');
+        });
     }
 
     /**
@@ -23,6 +33,11 @@ class RelationArticlesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign(['categorie_id']);
+            $table->dropColumn('categorie_id');
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
     }
 }
