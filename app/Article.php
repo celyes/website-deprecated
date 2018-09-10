c3e1fb53ec999e04e1faf1f3f230d6c28a330afa
ihahachi <iha2007hachi@gmail.com>
2018-09-10 21:01:15 +0100

    Add function  'get_last_article' to class Article

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    public function get_last_article($limit=100)
+    {
+        return $this->orderBy('created_at', 'desc')
+        ->take($limit)
+        ->get();
+    }
 }
