019ad56e9532c0139511e237ead02f52b1d4e47a
celyes <celyes02@gmail.com>
2018-09-11 15:14:31 +0100

    Create VoteQuestion model with migration

---
+++ database/migrations/2018_09_11_141259_create_vote_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVoteQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vote_questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vote_questions');
+    }
+}
