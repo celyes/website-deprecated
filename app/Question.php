62dc12c6b51d51f79b2e9a37f090b671caf4f252
ihahachi <iha2007hachi@gmail.com>
2018-09-17 20:24:41 +0100

    Add Function 'top_question_view' to class Question

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function top_question_view($limit=100)
+    {
+        return $this->orderBy('view', 'desc')
+        ->take($limit)
+        ->get();
+    }
 }
