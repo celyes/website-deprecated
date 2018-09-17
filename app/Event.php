53415285127962d9babba5688c09297af9e30536
ihahachi <iha2007hachi@gmail.com>
2018-09-17 21:56:27 +0100

    add function 'last_event' in class

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    public function last_event($limit=100)
+    {
+        return $this->orderBy('created_at', 'desc')
+        ->take($limit)
+        ->get();
+    }
 }
