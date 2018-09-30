68c5030c8f0d488478e65eeb55e76c7450a81629
Hamza BOUKHTAM <xu@live.fr>
2018-09-30 09:33:26 +0200

    updated to start real stuff

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import Index from './components/core/Index'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                Hello
-            </div>
-        );
+            <Fragment>
+                <CssBaseline />
+                {/*
+                    * TODO: should be more sophisticated,
+                    * ...implement logic for checking out if loged in for example*/
+                }
+                <Index />
+            </Fragment>
+        )
     }
 }
 
