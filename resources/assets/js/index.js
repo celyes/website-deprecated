175da314a80d43070189f05a4c2d0d847d9553ed
Hamza BOUKHTAM <xu@live.fr>
2018-09-30 09:27:31 +0200

    Integrated react router

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import {AppContainer} from 'react-hot-loader'
 
 import App from './App'
 
 ReactDOM.render(
     <AppContainer>
-        <App />
+        <BrowserRouter>
+            <Route component={App} />
+        </BrowserRouter>
     </AppContainer>,
     document.getElementById('app')
 )
