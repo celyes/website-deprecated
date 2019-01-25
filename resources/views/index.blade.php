06274850a8f0d0c5d11e4471f3ffebb431f43700
celyes <celyes02@gmail.com>
2019-01-25 21:47:02 +0100

    update file - index.blade.php app.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <h1>CODE CLUB DJELFA</h1>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <script src="{{ mix('js/index.js') }}" defer></script>
-</head>
-<body>
-<div id="app"></div>
-    @yield('content')
-</body>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <script src="{{ mix('js/index.js') }}" defer></script>
+    </head>
+    <body>
+        <div id="app">
+        @yield('content')
+        </div>
+    </body>
 </html>
